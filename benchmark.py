f5598157e28dc73e7c7f1d02e00c2dac155e8e56
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-09-19 10:00:37 -0400

    changes

---
+++ benchmark.py
@@ -0,0 +1,41 @@
+# Exercise 1:   Display your name in the console 
+
+
+print ('Jaylin Edouard')
+
+
+#excerise 2: create a variable called Favoriteshow 
+# and set it equal to the name of your favorite show
+
+Favoriteshow = {"FairyTail"}
+print (Favoriteshow)
+
+#excerise 3: create two variables called height & 
+# age , then display in the console, the product of them
+
+Height = ( 6 )
+Age = (16)
+
+print ( Height * Age) 
+#exercise 4 : display "im going to break " in the console 
+print ("Im going to break")
+
+#exercise 5 create a list of strings called "fav_food"
+Favfood = {" baked macaroni and cheese", "Chicken", "rice", "pasta"}
+print (Favfood)
+
+#exercise 6: create a conditional statement 
+#that checks if your age is greater than or equal to 21 
+#if trye then display "you can rent a car" other wise 
+#display "your beat"
+
+if Age > 21:
+    print ("You can rent a car")
+else:
+    print ("you can not rent a car at this age")
+
+#exercise 7: ask the user for their name, then display in the console "hello their_name"
+
+person = input('enter person name:') 
+
+print ("Hello my name is Jaylin. What is your name? " + " well hello Jaylin my Name is j" + person)
