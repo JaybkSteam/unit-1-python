7363f521c52959d1e6ae121a4ce1684c51731330
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-09-21 10:52:50 -0400

    changes

---
+++ listblitz.py
@@ -0,0 +1,67 @@
+#Task 1: Create a list
+#Create a list with 4 elements and print it.
+
+Cars = ["Camaro","Ferrari", "Supra","Nissan GT-R"]
+print(Cars) # made a varriable called list_cars and then put in 4 cars names and printed the list.
+
+
+
+#Task 2: Add Element to a List
+##Add an element to the end of the list. Print the updated 
+#list.
+
+Cars.append ("Lamborghini")
+print(Cars) # For this step i added the word .append to the end of my variable so i could add an another element to my list and just printed the list after.
+
+
+
+#Task 3: Remove Element from a List
+#Remove a specific element from the list. Print the 
+#updated list.
+Cars.remove("Nissan GT-R")
+print(Cars) #for this setp i used added the commadn .remove to my variable so i could remove an element from my list.
+
+
+
+#Task 4: Modify Element in a List
+#Modify an element at a specific index with a new value. 
+#Print the updated list.
+
+Cars2 = ["Bugatti", "Aston Martin"]
+Cars3 = ["Mustang " "Charger"]
+
+Cars4 = Cars2 + Cars3 
+
+print(Cars4) #For this step i creates 3 new variables called cars2, 
+#cars3 and cars4 so i decide to
+# make cars4 equal to cars2 and 
+# so when i click print cars4 it would print both cars2 and 3
+
+
+#Task 5: Append Multiple Elements to a List
+#Append multiple elements to the end of the list. Print 
+#the updated list. 
+
+
+
+Cars2.append ( "Aston Martin")
+Cars3.append("Charger")
+
+print (Cars)
+
+#Task 6: Delete Element at a Specific Index
+#Delete an element at a specific index. Print the updated 
+#list.
+
+
+
+
+#Task 7: Create a List with a Range of Numbers
+#Create a list containing numbers within a specified range 
+#(inclusive). Print the created list.
+print (Cars[-2])
+
+
+#Task 8: Extend a List
+#Extend the list with the elements of another list. Print 
+#the updated list.
