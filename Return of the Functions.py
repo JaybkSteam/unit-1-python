2a1b9af2c56902758c54639604926586d15ca302
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-10-13 10:55:47 -0400

    Changes

---
+++ Return of the Functions.py
@@ -0,0 +1,43 @@
+#Task 1: Calculate the Square of a Number
+#Write a function that takes an integer as an argument and returns its square.
+def square(n):
+  return n**2
+
+number = int(input('Please enter a number: '))
+number2 = square(number)
+print(number2)
+#Task 2: Calculate the Area of a Rectangle:
+#Write a function that takes the length and width of a rectangle and returns its area.
+def area_of_Rectangle(length, width):
+    return length * width
+length = float(input('Please Enter the Length of a Triangle: '))
+width = float(input('Please Enter the Width of a Triangle: '))
+
+# calculate the area of a Rectangle
+area = area_of_Rectangle(length, width)
+
+print("The Area of a Rectangle using", length, "and", width, " = ", area)
+
+#Task 3: Convert Temperature from Celsius to Fahrenheit:
+#Write a function that takes a temperature in Celsius and returns 
+#The equivalent temperature in Fahrenheit using the correct formula
+
+celsius_temp = 45
+fahrenheit_temp = (celsius_temp*9/5)+32
+print("The Fahrenheit equivalent of 45 celsius = ", fahrenheit_temp)
+
+
+
+
+
+
+#Task 4: Calculate the Average of Numbers:
+#Write a function that takes a list of numbers 
+#and returns the average (mean) of those numbers.
+from statistics import mean
+
+num_list = [30, 55, 3, 10, 2]
+
+average = mean(num_list)
+
+print("Average: ", round(average,3))
